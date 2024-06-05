3fa1dcd88ad631d21b947f2ffeac2d4407d938f5
uabdirasulm <ubbiniyazov04@gmail.com>
2024-06-05 17:17:22 +0500

    add sidebar

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,9 +1,12 @@
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="relative">
-      Navbar
+      <Navbar />
       <div className="flex">
-        Sidebar
+        <Sidebar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
           <div className="w-full">{children}</div>
         </section>
