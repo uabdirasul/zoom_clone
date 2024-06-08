57de9cee2a47ed54f7f36bd4539b06c4727852c3
uabdirasulm <ubbiniyazov04@gmail.com>
2024-06-08 19:01:56 +0500

    add 'create meeting' function

---
+++ actions/stream.actions.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import { currentUser } from "@clerk/nextjs/server";
+import { StreamClient } from "@stream-io/node-sdk";
+
+const apiKey = process.env.NEXT_PUBLIC_STEAM_API_KEY;
+const apiSecret = process.env.STREAM_SECRET_KEY;
+
+export const tokenProvider = async () => {
+  const user = await currentUser();
+
+  if (!user) throw new Error("User not found");
+  if (!apiKey) throw new Error("Missing Stream API Key");
+  if (!apiSecret) throw new Error("Missing Stream Secret Key");
+
+  const client = new StreamClient(apiKey, apiSecret);
+
+  const exp = Math.round(new Date().getTime() / 1000) + 60 * 60;
+  const issued = Math.floor(Date.now() / 1000) - 60;
+  const token = client.createToken(user.id, exp, issued);
+  return token;
+};
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,11 @@
+import StreamVideoProvider from "@/providers/StreamClientProvider";
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  return <main>{children}</main>;
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
 };
 
 export default RootLayout;
